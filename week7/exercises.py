e2db40ae676555b81f8d5176485e2349c9117ba6
Jadon <47489511+gonhunterx@users.noreply.github.com>
2024-02-27 09:02:53 -0500

    Create exercises.py

---
+++ week7/exercises.py
@@ -0,0 +1,17 @@
+import numpy as np 
+import pandas as pd 
+
+
+
+
+arr1 = np.ones((2, 3))
+print(arr1)
+
+arr2 = np.zeros((3, 3))
+print(arr2)
+
+arr3 = np.array()
+print(arr3)
+
+new_arr = np.array([x for x in range(1, 16)]).reshape(3, 5)
+print(new_arr)
